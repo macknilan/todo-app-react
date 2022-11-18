7e697f618d238057aa5d80c0113e6490ba1477e2
Rodolfo <rodolfougaldeochoa@gmail.com>
2022-11-17 23:48:35 -0600

    :tada: [WIP] initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        RobotoMonoRegular: ['Roboto Mono Regular', 'monospace'],
+      },
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+};
