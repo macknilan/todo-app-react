d94bed8bf922657bde736db065d3b24bfbf87342
Rodolfo <rodolfougaldeochoa@gmail.com>
2023-07-03 22:05:24 -0600

    :lipstick: [ADD] se agrega -tailwindcss- ...
    Se agrega -tailwindcss- y tipo de fuente Roboto Mono

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        RobotoMonoRegular: ['Roboto Mono Regular', 'monospace'],
+        RobotoMonoBold: ['Roboto Mono Bold', 'monospace'],
+        RobotoMonoItalic: ['Roboto Mono Italic', 'monospace'],
+        RobotoMonoMedium: ['Roboto Mono Medium', 'monospace'],
+        RobotoMonoBoldItalic: ['Roboto Mono BoldItalic', 'monospace'],
+        RobotoMonoExtraLight: ['Roboto Mono ExtraLight', 'monospace'],
+        RobotoMonoExtraLightItalic: ['Roboto Mono ExtraLightItalic', 'monospace'],
+        RobotoMonoLight: ['Roboto Mono Light', 'monospace'],
+        RobotoMonoLightItalic: ['Roboto Mono LightItalic', 'monospace'],
+        RobotoMonoMediumItalic: ['Roboto Mono MediumItalic', 'monospace'],
+        RobotoMonoSemiBold: ['Roboto Mono SemiBold', 'monospace'],
+        RobotoMonoSemiBoldItalic: ['Roboto Mono SemiBoldItalic', 'monospace'],
+        RobotoMonoThin: ['Roboto Mono Thin', 'monospace'],
+        RobotoMonoThinItalic: ['Roboto Mono ThinItalic', 'monospace'],
+      },
+    },
+    colors: {
+      my_bg_color: '#f7f7f7',
+    },
+  },
+  plugins: [],
+};
